355a67cc692ed1e365485fe7d6dec507cf95ef90
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-07-14 11:50:55 +0430

    test the object

--- functions.js
+++ functions.js
@@ -2,6 +2,11 @@ const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
   checkValue: (x) => x,
+  createUser: (firstName, lastName) =>
+    Object({
+      firstName,
+      lastName,
+    }),
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -19,3 +19,19 @@ test("test the isNull function; this function all times result equal to null", (
 test("test the checkValue function; this function all times return the argument value", () => {
   expect(functions.checkValue(null)).toBeFalsy();
 });
+
+// check the result is not falsy value
+test("test the checkValue function; this function all times return the argument value", () => {
+  expect(functions.checkValue("not falsy value")).not.toBeFalsy();
+});
+
+// check the success create user
+test("test the createUser function; pass 2 argument to this function and result should be must object of first name and last name", () => {
+  const firstName = "Amir Mostafa";
+  const lastName = "Haji Sadeghian";
+  expect(functions.createUser(firstName, lastName)).not.toBeFalsy();
+  expect(functions.createUser(firstName, lastName)).toEqual({
+    firstName,
+    lastName,
+  });
+});
